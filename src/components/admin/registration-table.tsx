f213943fceace9d9f4c4f3032d259ae0f3686bce
raafy <raafyshiham@gmail.com>
2024-08-30 04:21:27 +0800

    date update

--- src/components/admin/registration-table.tsx
+++ src/components/admin/registration-table.tsx
@@ -31,9 +31,10 @@ const RegistrationTable: React.FC<RegistrationTableProps> = ({
   onView,
   onDeleteRequest,
 }) => {
-  const formatDate = (date: Date) => {
+  const formatDate = (dateInput: Date | string) => {
+    const date = dateInput instanceof Date ? dateInput : new Date(dateInput);
     const day = String(date.getDate()).padStart(2, "0");
-    const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are zero-based
+    const month = String(date.getMonth() + 1).padStart(2, "0");
     const year = date.getFullYear();
 
     return `${day}/${month}/${year}`;
