453e08a9934f9d4138dff17183d339f477c0306f
raafy <raafyshiham@gmail.com>
2024-11-28 15:05:39 +0800

    fix: update dependencies and improve under construction page layout

--- src/components/site/under-construction.tsx
+++ src/components/site/under-construction.tsx
@@ -1,9 +1,20 @@
+import Image from "next/image";
+
 const UnderConstruction: React.FC = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold">
-        This site is under construction. Please check back later.
-      </h1>
+    <div className="flex min-h-dvh items-center justify-center bg-gray-100">
+      <div className="flex flex-col items-center gap-y-4 p-6">
+        <Image
+          src={"/logo.svg"}
+          alt="Logo"
+          width={200}
+          height={200}
+          className="lg:h-[300px] lg:w-[500px]"
+        />
+        <h1 className="text-center text-lg font-bold lg:text-4xl">
+          This site is under construction. Please check back later.
+        </h1>
+      </div>
     </div>
   );
 };
